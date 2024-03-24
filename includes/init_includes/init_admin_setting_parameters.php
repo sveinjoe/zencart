9ec3b8e4133cdad6c895c18aaa6d4c0638d56fc1
svein joe <sveinjoe@gmail.com>
2024-03-24 05:24:49 -0400

    后台如果没有指定配置那么新增

--- includes/init_includes/init_admin_setting_parameters.php
+++ includes/init_includes/init_admin_setting_parameters.php
@@ -1,40 +1,38 @@
 <?php
-if(!defined('HEADING_TITLE')){
-    $group_check = $db->Execute("SELECT min(configuration_group_id) as minid FROM " . TABLE_CONFIGURATION_GROUP . " WHERE 1");
-    if (!$group_check->EOF && $group_check->RecordCount() > 0 && $group_check->fields['minid'] > 0) {
-        $group_id = $group_check->fields['minid'];
-    }else{
-        die('get configuration group id error!');
+$arrConfigs = array(
+    'HEADING_TITLE' => array(
+        'configuration_title' => 'Heading Title In Index.',
+        'configuration_value' => 'Congratulations! You have successfully installed your Zen Cart&reg; E-Commerce Solution.',
+        'configuration_description' => 'Home page welcome message, displayed in the middle of the home page.',
+        'sort_order' => '1121',
+    ),
+    'TEXT_GREETING_GUEST' => array(
+        'configuration_title' => 'Welcome to Guest.',
+        'configuration_value' => 'Welcome <span class="greetUser">Guest!</span> Would you like to <a href="%s">log yourself in</a>?',
+        'configuration_description' => 'Home page welcome message to guest, displayed in the middle of the home page. ',
+        'sort_order' => '1122',
+    ),
+    'HEADER_SALES_TEXT' => array(
+        'configuration_title' => 'Sales text in headers.',
+        'configuration_value' => 'TagLine Here',
+        'configuration_description' => 'Sales Text in headers. It often show at right about the logo.',
+        'sort_order' => '1123',
+    ),
+);
+
+
+$group_check = $db->Execute("SELECT min(configuration_group_id) as minid FROM " . TABLE_CONFIGURATION_GROUP . " WHERE 1");
+if (!$group_check->EOF && $group_check->RecordCount() > 0 && $group_check->fields['minid'] > 0) {
+    $group_id = $group_check->fields['minid'];
+    //获取group id成功，尝试获取这个group下的全部常量
+    $con_results = $db->Execute("SELECT configuration_key FROM " . TABLE_CONFIGURATION . " WHERE configuration_group_id = " . $group_id . "");
+    $arrKeysExists = array();
+    foreach($con_results as $con_result){
+        $arrKeysExists[] = $con_result['configuration_key'];
     }
-    try{
-        $db->Execute("INSERT IGNORE INTO ".TABLE_CONFIGURATION." (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES('Heading Title In Index.', 'HEADING_TITLE', 'Congratulations! You have successfully installed your Zen Cart&reg; E-Commerce Solution.', 'Home page welcome message, displayed in the middle of the home page. ', ".$group_id.", '1121', NULL, now(), NULL, '')");
-    }catch(Exception $e){
-        null;
-    }
-}
-if(!defined('TEXT_GREETING_GUEST')){
-    $group_check = $db->Execute("SELECT min(configuration_group_id) as minid FROM " . TABLE_CONFIGURATION_GROUP . " WHERE 1");
-    if (!$group_check->EOF && $group_check->RecordCount() > 0 && $group_check->fields['minid'] > 0) {
-        $group_id = $group_check->fields['minid'];
-    }else{
-        die('get configuration group id error!');
-    }
-    try{
-        $db->Execute("INSERT IGNORE INTO ".TABLE_CONFIGURATION." (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES('Welcome to Guest.', 'TEXT_GREETING_GUEST', 'Welcome <span class=\"greetUser\">Guest!</span> Would you like to <a href=\"%s\">log yourself in</a>?', 'Home page welcome message to guest, displayed in the middle of the home page. ', ".$group_id.", '1122', NULL, now(), NULL, '')");
-    }catch(Exception $e){
-        null;
-    }
-}
-if(!defined('HEADER_SALES_TEXT')){
-    $group_check = $db->Execute("SELECT min(configuration_group_id) as minid FROM " . TABLE_CONFIGURATION_GROUP . " WHERE 1");
-    if (!$group_check->EOF && $group_check->RecordCount() > 0 && $group_check->fields['minid'] > 0) {
-        $group_id = $group_check->fields['minid'];
-    }else{
-        die('get configuration group id error!');
-    }
-    try{
-        $db->Execute("INSERT IGNORE INTO ".TABLE_CONFIGURATION." (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES('Sales text in headers.', 'HEADER_SALES_TEXT', 'TagLine Here', 'Sales Text in headers. It often show at right about the logo.', ".$group_id.", '1123', NULL, now(), NULL, '')");
-    }catch(Exception $e){
-        null;
+    foreach($arrConfigs as $key => $configItem){
+        if(!in_array($key, $arrKeysExists)){
+            $db->Execute("INSERT IGNORE INTO ".TABLE_CONFIGURATION." (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES('" . $configItem['configuration_title'] . "', '" . $key . "', '" . $configItem['configuration_value'] . "', '" . $configItem['configuration_description'] . "', ".$group_id.", '" . $configItem['sort_order'] . "', NULL, now(), NULL, '')");
+        }
     }
 }
