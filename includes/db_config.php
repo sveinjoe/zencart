1d98dfa0f9d4f13212722773bd1b2af9138de7dc
svein joe <sveinjoe@gmail.com>
2024-05-09 11:10:52 -0400

    Update db_config.php

--- includes/db_config.php
+++ includes/db_config.php
@@ -3,6 +3,6 @@ define('DB_TYPE', 'mysql'); // always 'mysql'
 define('DB_PREFIX', 'zen_'); // prefix for database table names -- preferred to be left empty
 define('DB_CHARSET', 'utf8mb4'); // 'utf8mb4' or older 'utf8' / 'latin1' are most common
 define('DB_SERVER', 'localhost');  // address of your db server
-define('DB_SERVER_USERNAME', 'zencart158a_com');
+define('DB_SERVER_USERNAME', 'zencart158a_pass');
 define('DB_SERVER_PASSWORD', 'zencart158a_pass');
 define('DB_DATABASE', 'zencart158a_com');
